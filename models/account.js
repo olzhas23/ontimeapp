343677325bba84abdad00a0cb61e4f8802e05c67
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2015-12-30 16:05:20 -0500

    updated app1.js file

--- models/account.js
+++ models/account.js
@@ -6,7 +6,7 @@ var Account = new Schema({
     username: String,
     password: String
 });
-
+console.log("this is account echo");
 Account.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('Account', Account);
--- public/js/app.jsx
+++ public/js/app.jsx
@@ -1,124 +1,39 @@
-var Header = React.createClass({
-  render: function(){
+import React from 'react';
+import DatePicker from '/react-toolbox/lib/date_picker';
+
+
+
+const datetime = new Date(2015, 10, 16);
+const min_datetime = new Date(new Date(datetime).setDate(8));
+const max_datetime = new Date(new Date(datetime).setDate(24));
+datetime.setHours(17);
+datetime.setMinutes(28);
+
+class DatePickerTest extends React.Component {
+  state = {date2: datetime};
+
+  handleChange = (item, value) => {
+    this.setState({...this.state, [item]: value});
+  };
+
+  render () {
     return (
-      <h1>Message Board</h1>
-    )
+      <section>
+        <DatePicker
+          label='Birthdate'
+          onChange={this.handleChange.bind(this, 'date1')}
+          value={this.state.date1}
+        />
+
+        <DatePicker
+          label='Expiration date'
+          minDate={min_datetime}
+          onChange={this.handleChange.bind(this, 'date2')}
+          value={this.state.date2}
+        />
+      </section>
+    );
   }
-})
+}
 
-var Footer = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <hr />
-        <div className="row-fluid">
-          <div className="span12"> 
-            <div>The React.js Course by Azat (<a href="http://twitter.com/azat_co" target="_blank">@azat_co</a>)</div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-})
-
-
-var MessageList = React.createClass({
-  render: function(){
-    var messages = this.props.messages //
-    if (!messages.length>0) return (
-      <tr>
-        <td colspan="2">No messages yet</td>
-      </tr>
-    )
-    return (
-      <table className="table ">
-        <caption>Messages</caption>
-        <thead>
-          <tr>
-            <th className="span2">Name</th>
-            <th className="span10">Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          {messages.map(function(message){
-            return (
-              <tr key={message._id}>
-                <td className="span2">{message.name}</td>
-                <td className="span10">{message.message}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    )
-  }
-})
-
-
-var NewMessage = React.createClass({
-  addMessage: function(){
-    var fD = React.findDOMNode
-    this.props.addMessageCb({
-      name: fD(this.refs.name).value,
-      message: fD(this.refs.message).value
-    })
-    fD(this.refs.name).value = ''
-    fD(this.refs.message).value = ''
-  },
-  keyup: function (e) {
-    if (e.keyCode == 13) return this.addMessage()
-  },
-  render: function(){
-    return (
-      <div className="row-fluid" id="new-message">
-        <div className="span12">
-          <form className="well form-inline" onKeyUp={this.keyup}>
-            <input type="text" name="username" className="input-small" placeholder="Name" ref="name"/>
-            <input type="text" name="message" className="input-small" placeholder="Post Message!" ref="message" />
-            <a id="send" className="btn btn-primary" onClick={this.addMessage}>POST</a>
-          </form>
-        </div>
-      </div>
-    )
-  }
-})
-
-var url = 'localhost:3000/messages'
-
-var MessageBoard = React.createClass({
-  getInitialState: function(){
-    return {messages: [{_id: 1, name: 'Name', message: 'Message'}]}
-  },
-  componentWillMount: function(){
-    var _this = this
-    $.getJSON(url, function(result){
-      if(!result || !result.length){
-        return
-      }
-      _this.setState({ messages: result })
-    })
-  },
-  addMessage: function(message){
-    var messages = this.state.messages
-    var _this = this
-    $.post(url, message, function(data) {
-      if(!data){
-        return console.error('Failed to save')
-      }
-      messages.unshift(data)
-      _this.setState({messages: messages})
-    })
-  },
-  render: function(){
-    return (
-      <div>
-        <NewMessage messages={this.state.messages} addMessageCb={this.addMessage} />
-        <MessageList messages={this.state.messages} />
-      </div>
-    )
-  }
-})
-
-React.render(<Header />, document.getElementById('header'))
-React.render(<Footer />, document.getElementById('footer'))
-React.render(<MessageBoard />, document.getElementById('message-board'))
+return <DatePickerTest />;
